5b39b1f97acbbc05c406ac89d64825522909b95a
ArthurBerman <arthurberman94@gmail.com>
2025-03-16 20:26:35 -0400

    Refine all elements

--- ball.gd
+++ ball.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
     body_entered.connect(_on_contact)
 
 func _on_contact(contact: Node3D):
-    pass
+    $Audio.play()
--- tables/flipper.gd
+++ tables/flipper.gd
@@ -2,13 +2,20 @@ extends HingeJoint3D
 
 
 @export var action: String = "flip_left"
+
+func _ready() -> void:
+	self["motor/enable"] = true
+	self["motor/target_velocity"] = 10
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed(action):
-		self["motor/target_velocity"] = -70
+		self["motor/target_velocity"] = -60
 		self["motor/enable"] = true
+		if $Audio:
+			$Audio.play()
 	elif Input.is_action_pressed(action):
-		self["motor/enable"] = false
-		
+		self["motor/target_velocity"] = -30
+		self["motor/enable"] = true
+
 	if Input.is_action_just_released(action):
 		self["motor/enable"] = true
 		self["motor/target_velocity"] = 10
